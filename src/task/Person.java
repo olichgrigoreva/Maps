1333dda7c691fc5dbad690c9c2a72f76daa486ee
Olga <aboutrocky94@gmail.com>
2020-09-08 18:42:05 +0300

    init

--- src/task/Person.java
+++ src/task/Person.java
@@ -24,4 +24,18 @@ public class Person {
         return  "name='" + name + '\'' +
                 ", lastName='" + lastName + '\'';
     }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        Person person = (Person) o;
+        return Objects.equals(name, person.name) &&
+                Objects.equals(lastName, person.lastName);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(name, lastName);
+    }
 }
